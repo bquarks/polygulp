bc3d5efb23ea6b65654f3ffc717c4be429241a5e
oscarferrandiz <oscar.ferrandiz@bq.com>
2015-07-08 16:12:11 +0200

    added gulp-strip-debug to dist task

--- tasks/dist.js
+++ tasks/dist.js
@@ -36,6 +36,7 @@ module.exports = function(gulp, config) {
         var svg = $.svgmin().pipe($.svgstore()).pipe(gulp.dest(config.PATHS.dist + '/assets/svg/sprite'));
         var index = $.htmlmin(options.htmlmin);
         var styles = $.postcss(config.postcssProcessors);
+        var js = $.stripDebug();
 
         var bower = gulp.src('bower_components/**/*.*').pipe(gulp.dest(config.PATHS.dist + '/bower_components'));
 
@@ -43,6 +44,7 @@ module.exports = function(gulp, config) {
             .pipe($.if(/.*\.(?:jpg|gif|png|bmp)$/, $.imagemin(options.imagemin)))
             .pipe($.if('*.svg', svg))
             .pipe($.if('*.css', styles))
+            .pipe($.if('*.js', js))
             // .pipe($.if(/(?:elements|pages)\/.+\/.+\.html/, $.vulcanize(options.vulcanize)))
             .pipe($.if(/index\.html/, index))
             .pipe(gulp.dest(config.PATHS.dist));
